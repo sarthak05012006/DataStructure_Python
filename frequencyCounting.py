52037683a3052fe1ef4000735a4a890a96f71a74
Sarthak Dewangan <72980936+sarthak05012006@users.noreply.github.com>
2026-09-03 21:39:08 +0530

    Delete frequencyCounting.py

--- frequencyCounting.py
+++
@@ -1,9 +0,0 @@
-def count_frequency(arr):
-    freq_map = {}
-    for item in arr:
-        # If item exists, increment it; otherwise, initialize it to 0 and add 1
-        freq_map[item] = freq_map.get(item, 0) + 1
-    return freq_map
-
-fruits = ["apple", "banana", "apple", "cherry", "banana", "apple"]
-print(count_frequency(fruits))  
